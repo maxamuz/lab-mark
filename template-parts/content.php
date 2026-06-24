0ff6c8e3ee3513834e9b1bcab71a8a5595ce662d
maxamuz <laptevx@gmail.com>
2026-06-24 16:20:14 +0300

    Убираем заголовки одиночных записей

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,24 +11,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-header">
-		<?php
-		if (is_singular()) :
-			the_title('<h1 class="entry-title">', '</h1>');
-		else :
-			the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-		endif;
-
-		if ('post' === get_post_type()) :
-		?>
-			<div class="entry-meta">
-				<?php
-				lab_mark_posted_on();
-				lab_mark_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</div><!-- .entry-header -->
 
 	<?php // lab_mark_post_thumbnail(); 
 	?>
